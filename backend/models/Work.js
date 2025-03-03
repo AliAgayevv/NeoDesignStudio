1dfef1b9cdcd178f1f4af1ec59675c129e5f0273
aliagayevv <aghayev_233@mail.ru>
2025-03-03 17:21:45 +0400

    deleted page key in work schema

--- backend/models/Work.js
+++ backend/models/Work.js
@@ -2,8 +2,6 @@ const mongoose = require("mongoose");
 
 const ProjectSchema = new mongoose.Schema({
   projectId: { type: String, required: true, unique: true }, // Benzersiz proje ID'si
-  page: { type: String, required: true }, // Sayfa türü
-  pageType: { type: String, default: "static" }, // Sayfa tipi
   images: { type: [String], default: [] }, // Fotoğraf URL'leri (dillerden bağımsız)
   content: {
     az: {
