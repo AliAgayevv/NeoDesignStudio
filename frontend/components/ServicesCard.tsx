ae61f90d06232337c0d92e09c542048d19028354
aliagayevv <aghayev_233@mail.ru>
2025-03-31 17:50:28 +0400

    FEATURES>PROJECTS>Category added, services real photo added, backend get a work by category

--- frontend/components/ServicesCard.tsx
+++ frontend/components/ServicesCard.tsx
@@ -1,30 +1,36 @@
-import React from 'react'
-import Image from 'next/image'
-import HeaderTitle from './HeaderTitle'
+import React from "react";
+import Image from "next/image";
+import HeaderTitle from "./HeaderTitle";
 
 interface ServicesCardProps {
-  img: any
-  text: any
-  isReversed?: boolean
+  img: any;
+  text: any;
+  isReversed?: boolean;
 }
 
-const ServicesCard: React.FC<ServicesCardProps> = ({ img, text, isReversed = false }) => {
+const ServicesCard: React.FC<ServicesCardProps> = ({
+  img,
+  text,
+  isReversed = false,
+}) => {
   return (
     <div
       className={`flex h-[99vh] translate-y-4 flex-col items-center ${
-        isReversed ? 'flex-col-reverse' : ''
+        isReversed ? "flex-col-reverse" : ""
       }`}
     >
-      <Image
+      <img
         src={img}
         alt={`${text} Image`}
         className={`aspect-[9/16] max-h-[60vh] w-full min-w-[30vw] max-w-[30vw] rounded-3xl object-cover ${
-          isReversed ? 'translate-y-44' : ''
+          isReversed ? "translate-y-44" : ""
         }`}
       />
-      <HeaderTitle classname={`my-auto ${!isReversed ? 'translate-y-40' : ''}`}>{text}</HeaderTitle>
+      <HeaderTitle classname={`my-auto ${!isReversed ? "translate-y-40" : ""}`}>
+        {text}
+      </HeaderTitle>
     </div>
-  )
-}
+  );
+};
 
-export default ServicesCard
+export default ServicesCard;
