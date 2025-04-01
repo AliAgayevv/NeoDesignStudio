62e25a27f2a6ca4dac80eed3dfeddcf09c5a186f
aliagayevv <aghayev_233@mail.ru>
2025-04-01 18:10:22 +0400

    cors fixed

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,8 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:3000",
   "https://demo-neodesignstudio.vercel.app",
+  "http://45.85.146.73:3000",
+  "https://45.85.146.73:3000",
 ];
 
 app.use(
