63db26527ca455d6dc5a690fb24779f305ca4700
aliagayevv <aghayev_233@mail.ru>
2025-04-01 18:32:35 +0400

    Next image config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["localhost", "45.85.146.73:4000", "example.com"],
+    domains: ["localhost", "http://45.85.146.73:4000", "http://45.85.146.73"],
   },
 };
 
