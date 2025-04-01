1ad2ef5d14565307f63749d11f0fa5c2d478a1f8
aliagayevv <aghayev_233@mail.ru>
2025-04-01 18:27:21 +0400

    db error fix

--- backend/db.js
+++ backend/db.js
@@ -9,7 +9,7 @@ const connectDB = async () => {
       throw new Error("MONGO_URI is not defined in .env file");
     }
 
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect("mongodb://localhost:27017/neoDesign");
 
     console.log("✅✅✅ MongoDB Connected ✅✅✅");
 
