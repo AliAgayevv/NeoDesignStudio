231fb7b8290cd8d24dda54796f379f1d2fefb504
aliagayevv <aghayev_233@mail.ru>
2025-04-06 16:26:01 +0400

    frontend services card updated for hover scael

--- frontend/components/ServicesCard.tsx
+++ frontend/components/ServicesCard.tsx
@@ -18,13 +18,17 @@ const ServicesCard: React.FC<ServicesCardProps> = ({
         isReversed ? "flex-col-reverse" : ""
       }`}
     >
-      <img
-        src={img}
-        alt={`${text} Image`}
-        className={`aspect-[9/16] max-h-[60vh] w-full min-w-[30vw] max-w-[30vw] rounded-3xl object-cover ${
+      <div
+        className={`overflow-hidden rounded-3xl w-full min-w-[30vw] max-w-[30vw] ${
           isReversed ? "translate-y-44" : ""
         }`}
-      />
+      >
+        <img
+          src={img}
+          alt={`${text} Image`}
+          className={`aspect-[9/16] max-h-[60vh] w-full object-cover transition-transform duration-300 md:hover:scale-110`}
+        />
+      </div>
       <HeaderTitle classname={`my-auto ${!isReversed ? "translate-y-40" : ""}`}>
         {text}
       </HeaderTitle>
