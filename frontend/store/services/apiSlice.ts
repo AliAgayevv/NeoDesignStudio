beba19390e00d88f1184ea2d0485c56400ec3180
aliagayevv <aghayev_233@mail.ru>
2025-04-08 19:37:09 +0400

    api slice changed for nginx setup?

--- frontend/store/services/apiSlice.ts
+++ frontend/store/services/apiSlice.ts
@@ -1,9 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://45.85.146.73:4000/api",
+    baseUrl: "/api",
   }),
   endpoints: () => ({}),
 });
