8c7adcdaa891a6841c2baae9fe474d6281e2f79c
aliagayevv <aghayev_233@mail.ru>
2025-04-10 15:04:02 +0400

    section header title animation added

--- frontend/components/SectionHeaderTitle.tsx
+++ frontend/components/SectionHeaderTitle.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Montserrat } from "next/font/google";
+import { motion } from "framer-motion";
 
 interface SectionHeaderTitleProps {
   children: React.ReactNode;
@@ -11,14 +12,17 @@ const SectionHeaderTitle: React.FC<SectionHeaderTitleProps> = ({
   children,
 }) => {
   return (
-    <h1
+    <motion.h1
+      initial={{ opacity: 0, x: -50 }}
+      whileInView={{ opacity: 1, x: 0 }}
+      transition={{ duration: 0.6, ease: "easeOut" }}
+      viewport={{ once: true, amount: 0.3 }}
       className={`header_text text-clip font-[600] break-words uppercase tracking-wider ${montserratFont700.className}
-        md:bg-gradient-to-r md:from-white  md:to-black md:text-transparent bg-clip-text
-        
+        md:bg-gradient-to-r md:from-white md:to-black md:text-transparent bg-clip-text
       `}
     >
       {children}
-    </h1>
+    </motion.h1>
   );
 };
 
